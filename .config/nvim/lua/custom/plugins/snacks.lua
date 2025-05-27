c391874cdd3ff3fc9454d4c919eca81e0acd2ea5
Zac Johnson <zacjohnson@protonmail.com>
2025-05-27 13:52:16 -0400

    add image ability to snacks.nvim

--- .config/nvim/lua/custom/plugins/snacks.lua
+++ .config/nvim/lua/custom/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
   opts = {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
+    image = {},
     indent = { enabled = true },
     input = { enabled = true },
     notifier = {
