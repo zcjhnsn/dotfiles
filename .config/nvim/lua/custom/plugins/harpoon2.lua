3e8653e0cc766f676d9442061faa2fd09e019dc8
Zac Johnson <zac.johnson@locushealth.com>
2026-01-12 16:07:56 -0500

    reconfigure harpoon2

--- .config/nvim/lua/custom/plugins/harpoon2.lua
+++ .config/nvim/lua/custom/plugins/harpoon2.lua
@@ -3,72 +3,38 @@ return {
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    require('harpoon'):setup()
+    local harpoon = require 'harpoon'
+    harpoon:setup()
+
+    -- add current file to harpoon list
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list():add()
+    end, { desc = 'harpoon file' })
+
+    -- toggle harpoon quick menu
+    vim.keymap.set('n', '<leader>A', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'harpoon quick menu' })
+
+    -- Map <leader>1 to <leader>5 to select harpooned files 1 to 5
+    for i = 1, 5 do
+      vim.keymap.set('n', '<leader>' .. i, function()
+        harpoon:list():select(i)
+      end, { desc = 'harpoon to file ' .. i })
+    end
+
+    -- navigate to previous/next harpooned file with wrapping
+    vim.keymap.set('n', '<C-p>', function()
+      harpoon:list():prev { ui_nav_wrap = true }
+    end, { desc = 'harpoon previous file' })
+
+    vim.keymap.set('n', '<C-n>', function()
+      harpoon:list():next { ui_nav_wrap = true }
+    end, { desc = 'harpoon next file' })
+
+    -- clear harpoon list
+    vim.keymap.set('n', '<leader>hc', function()
+      harpoon:list():clear()
+    end, { desc = 'harpoon clear list' })
   end,
-  keys = {
-    {
-      '<leader>A',
-      function()
-        require('harpoon'):list():add()
-      end,
-      desc = 'harpoon file',
-    },
-    {
-      '<leader>a',
-      function()
-        local harpoon = require 'harpoon'
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end,
-      desc = 'harpoon quick menu',
-    },
-    {
-      '<leader>1',
-      function()
-        require('harpoon'):list():select(1)
-      end,
-      desc = 'harpoon to file 1',
-    },
-    {
-      '<leader>2',
-      function()
-        require('harpoon'):list():select(2)
-      end,
-      desc = 'harpoon to file 2',
-    },
-    {
-      '<leader>3',
-      function()
-        require('harpoon'):list():select(3)
-      end,
-      desc = 'harpoon to file 3',
-    },
-    {
-      '<leader>4',
-      function()
-        require('harpoon'):list():select(4)
-      end,
-      desc = 'harpoon to file 4',
-    },
-    {
-      '<leader>5',
-      function()
-        require('harpoon'):list():select(5)
-      end,
-      desc = 'harpoon to file 5',
-    },
-    {
-      '<C-k>',
-      function()
-        require('harpoon'):list():prev()
-      end,
-      desc = 'harpoon prev file',
-    },
-    {
-      '<C-l>',
-      function()
-        require('harpoon'):list():next()
-      end,
-      desc = 'harpoon next file',
-    },
-  },
 }
