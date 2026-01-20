8dd41ac32e725d49c43eae152eafd4b191d435dc
Zac Johnson <zacjohnson@protonmail.com>
2026-01-19 22:20:53 -0500

    rose pine nvim + other themes

--- .config/nvim/lua/custom/disabled/matteblack.lua
+++ .config/nvim/lua/custom/disabled/matteblack.lua
@@ -3,6 +3,7 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'matteblack'
+    require('matteblack').colorscheme()
+    -- vim.cmd.colorscheme 'matteblack'
   end,
 }
