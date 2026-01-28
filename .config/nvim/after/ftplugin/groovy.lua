3cd1953f9cfa931bd9ff06b0b082f22ef4421f04
Zac Johnson <zac.johnson@locushealth.com>
2026-01-28 11:30:24 -0500

    gradle ftplugin yuck

---
+++ .config/nvim/after/ftplugin/groovy.lua
@@ -0,0 +1,5 @@
+vim.opt_local.tabstop = 4 -- size of a hard tabstop (ts).
+vim.opt_local.shiftwidth = 4 -- size of an indentation (sw).
+vim.opt_local.expandtab = true -- always uses spaces instead of tab characters (et).
+vim.opt_local.softtabstop = 4 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
+vim.opt_local.textwidth = 180 -- use 180 cols before autoindenting lines on save
